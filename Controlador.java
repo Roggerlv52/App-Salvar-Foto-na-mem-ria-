0ee4a87505753b0253db77ee2d17d5e22115845d
Rogger <96388665+Roggerlv52@users.noreply.github.com>
2025-01-12 14:39:29 +0000

    Update Controlador.java
    Função da interface atualizada.

--- Controlador.java
+++ Controlador.java
@@ -1,10 +1,6 @@
 package com.rogger.test;
-
-import android.app.Fragment;
 import android.view.View;
 
 public interface Controlador {
-	interface Controler<View> {
-		public void setClickListener();
-	}
+	public void StarCamera();
 }
