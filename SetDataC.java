000e91833278d920e25856174f7ae7f3bd1cc2ef
Rogger <96388665+Roggerlv52@users.noreply.github.com>
2025-01-12 21:31:35 +0000

    Update SetDataC.java

--- SetDataC.java
+++ SetDataC.java
@@ -1,26 +1,69 @@
 package com.rogger.test;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class SetDataC {
 	
+	private EditText edtName, edtQuant, edtPrice;
 	private Context context;
 	private String name = "";
-	private String barcode ="";
+	private int amount;
+	private float price;
+	private String barcode = "";
 	
-	public SetDataC(){
-		
+	public SetDataC() {
+
 	}
-	public void setContext(Context c){
-		this.context = c;
+
+	public void setEdtName(EditText edt) {
+		this.name = edt.getText().toString();
 	}
-	public void setBarcode(String b){
-		this.barcode = b;
+
+	public void setEdtAmount(EditText edt) {
+
+		this.amount = Integer.parseInt(edt.getText().toString());
 	}
-	public String getName(){
+
+	public void setEdtPrice(EditText edt) {
+		edt.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				String obj = edt.getText().toString();
+				if (obj.isEmpty()) {
+					price = 0.0f;
+					return;
+				}
+				price = Float.parseFloat(obj);
+			}
+
+		});
+		//this.price = Float.parseFloat(edt.getText().toString());
+	}
+
+	public int getAmount() {
+		return this.amount;
+	}
+
+	public String getName() {
 		return this.name;
 	}
-	public String getBarcode(){
+
+	public void setBarcode(String b) {
+		this.barcode = b;
+	}
+
+	public String getBarcode() {
 		return this.barcode;
 	}
 
