923b1d7073b726498e1abe0c6adc6d08a9c225f5
Rogger <96388665+Roggerlv52@users.noreply.github.com>
2025-01-13 21:21:23 +0000

    Update SetDataC.java

--- SetDataC.java
+++ SetDataC.java
@@ -23,7 +23,30 @@ public class SetDataC {
 
 	public void setEdtAmount(EditText edt) {
 
-		this.amount = Integer.parseInt(edt.getText().toString());
+		edt.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				String obj = edt.getText().toString();
+				int i = 0;
+				if (!obj.isEmpty()) {
+
+					i = Integer.parseInt(obj);
+					for (int i2 = 1; i2 <= i; i2++) {
+						soma += price;
+					}
+					DecimalFormat decimalFormat = new DecimalFormat("#.00");
+					totPrice = Float.parseFloat(decimalFormat.format(soma));
+				}
+			}
+		});
 	}
 	public void setEdtPrice(EditText edt) {
 		edt.addTextChangedListener(new TextWatcher() {
@@ -38,11 +61,16 @@ public class SetDataC {
 			@Override
 			public void afterTextChanged(Editable s) {
 				String obj = edt.getText().toString();
-				if (obj.isEmpty()) {
-					price = 0.0f;
-					return;
+				try {
+
+					if (obj.isEmpty() && obj != null) {
+						price = 0.0f;
+					} else {
+						price = Float.parseFloat(obj);
+					}
+				} catch (Exception e) {
+					Toast.makeText(context, "Valor invalido! (" + obj + ")", Toast.LENGTH_SHORT).show();
 				}
-				price = Float.parseFloat(obj);
 			}
 
 		})
